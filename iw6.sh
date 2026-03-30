4332bda2743c98c512a67791e757da185e0a4491
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2026-03-30 01:01:04 -0500

    Create iw6.sh

---
+++ iw6.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+STEAM_COMPAT_CLIENT_INSTALL_PATH="$HOME/.local/share/Steam" \
+STEAM_COMPAT_DATA_PATH="$HOME/.proton/iw6" \
+"$HOME/.local/share/Steam/compatibilitytools.d/GE-Proton10-34/proton" run ./iw6-mod.exe -multiplayer
